8b94d347350919276d40e3b1063eb80233cbd2de
Oladunjoye Victor Xpan <xpanvictor@gmail.com>
2022-01-07 21:58:24 +0100

    Updates

---
+++ precache-manifest.4ab1558c3ddbbb7103cdd86f77dddfe8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c95d691c8c4fbea2404fc639aeb62f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f62e9cf8d07b9acf7954",
+    "url": "/static/css/main.c2e8e41c.chunk.css"
+  },
+  {
+    "revision": "ab98e27fdf62ae5a3c61",
+    "url": "/static/js/2.3ea39a12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ea39a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62e9cf8d07b9acf7954",
+    "url": "/static/js/main.e57241aa.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  },
+  {
+    "revision": "e111a03f2827e1092ab162da6d8df860",
+    "url": "/static/media/1.e111a03f.png"
+  },
+  {
+    "revision": "6d82d8d63ccd7128f5d2277aaaf8854d",
+    "url": "/static/media/2.6d82d8d6.png"
+  },
+  {
+    "revision": "892afe8ebc60ceb02f26650b33fc2881",
+    "url": "/static/media/3.892afe8e.png"
+  },
+  {
+    "revision": "30c09f10199b55fc78eae972abe37145",
+    "url": "/static/media/4.30c09f10.png"
+  },
+  {
+    "revision": "8980e52edcd459818c75df07c66dc8ca",
+    "url": "/static/media/5.8980e52e.png"
+  },
+  {
+    "revision": "098106544b07a1d1ad391862eba59b97",
+    "url": "/static/media/6.09810654.png"
+  },
+  {
+    "revision": "8c7aa454a35e7b2686eef6b6cfebba9c",
+    "url": "/static/media/7.8c7aa454.png"
+  },
+  {
+    "revision": "e0475cb133cfca4d6dcb71608796c44e",
+    "url": "/static/media/8.e0475cb1.png"
+  },
+  {
+    "revision": "07472bcede1266bd1773be34465d3385",
+    "url": "/static/media/9.07472bce.png"
+  },
+  {
+    "revision": "40000a14460bf333e592b0641e83b6f5",
+    "url": "/static/media/cancel.40000a14.svg"
+  },
+  {
+    "revision": "752595b66aaca1ac2a2620322be4d0bf",
+    "url": "/static/media/fb.752595b6.svg"
+  },
+  {
+    "revision": "745c10fe059adde71b4cae5212650e03",
+    "url": "/static/media/footballplayer.745c10fe.png"
+  },
+  {
+    "revision": "cae275f9816afaa83e2893e70a41875b",
+    "url": "/static/media/globe.cae275f9.jpg"
+  },
+  {
+    "revision": "9119a5d6b2c7a38ddd77ff0028f564c3",
+    "url": "/static/media/hand.9119a5d6.svg"
+  },
+  {
+    "revision": "466475c700cd8a3a3648815b4cb4ff7e",
+    "url": "/static/media/ig.466475c7.svg"
+  },
+  {
+    "revision": "8dd39fa528b0320268929b08c335e6f9",
+    "url": "/static/media/logo.8dd39fa5.png"
+  },
+  {
+    "revision": "486901a2f09af49fbf8b1f04d21b1c9b",
+    "url": "/static/media/menu.486901a2.svg"
+  },
+  {
+    "revision": "23d7da442efa9fd5738031a8c61423d3",
+    "url": "/static/media/ms.23d7da44.svg"
+  },
+  {
+    "revision": "3487eea750fa2de2ee41606d78584a15",
+    "url": "/static/media/plane1.3487eea7.png"
+  },
+  {
+    "revision": "46fd18e95d196e2441d86db25fcc8622",
+    "url": "/static/media/plane2.46fd18e9.jpg"
+  },
+  {
+    "revision": "55318b5940bafa47d467e44cdd11b23a",
+    "url": "/static/media/wa.55318b59.svg"
+  }
+]);
